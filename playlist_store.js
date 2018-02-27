95798a5e84c7c6da84e2c7b0f98567c90c876f73
Ian <ian (at) ianrobinson.net>
2018-02-27 12:46:37 -0500

    Solved buttonClick problem. Time to test presistence

--- playlist_store.js
+++ playlist_store.js
@@ -1,39 +1,48 @@
+window.onload = init;
+
 function init() { 
-  var button = document.getElementById('addButton');
-  button.onClick = handleButtonClick;
+  var button = document.getElementById("addButton");
+  button.onclick = handleButtonClick;
   loadPlaylist();
 }
 
+function handleButtonClick() {
+  console.log('handlebuttonclicked')
+  var textInput = document.getElementById('songTextInput');
+  var songName = textInput.value;
+  var li = document.createElement('li');
+  li.innerHTML = songName;
+  var ul = document.getElementById("list");
+  ul.appendChild(li);
+  save(songName);
+}
+
 function save(item) {
-  console.log('save function called');
   var playlistArray = getStoreArray("playlist");
   playlistArray.push(item);
   localStorage.setItem("playlist", JSON.stringify(playlistArray));
-  console.log(playlistArray);
+  console.log(playlistArray, "<-playlist array in save()");
 }
 
 function loadPlaylist() {
-  console.log('loadPlaylistCalled');
   var playlistArray = getSavedSongs();
-  var ul = document.getElementById("playlist");
+  var ul = document.getElementById("list");
   
   if (playlistArray != null) {
     for (var i = 0; i < playlistArray.length; i += 1) {
       var li = document.createElement("li");
       li.innderHTML = playlistArray[i];
-      ul.appendChile(li);
+      ul.appendChild(li);
     }
   }
   console.log('playlist loaded');
 }
 
 function getSavedSongs() {
-  console.log('getSavedSongs called');
   return getStoreArray("playlist");
 }
 
 function getStoreArray(key) {
-  console.log('getStore Array Called');
   var playlistArray = localStorage.getItem(key);
   if (playlistArray === null || playlistArray === "") {
     playlistArray = [];
@@ -41,15 +50,6 @@ function getStoreArray(key) {
     playlistArray = JSON.parse(playlistArray);
   }
   
+  console.log(playlistArray, "<- getStoreArray() called this is playlistArray");
   return playlistArray;
-}
-
-function handleButtonClick() {
-  var inputValue = document.getElementById('songTextInput');
-  var songName = inputValue.value;
-  var li = createElement('id');
-  li.innerHTML = songName;
-  var ul = document.getElementById('playlist');
-  ul.appendChild(li);
-  save(songName);
 }
